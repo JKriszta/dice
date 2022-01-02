81541fb819bae1ac516a7a6432423bfbc0532482
JKriszta <jozsakriszta@gmail.com>
2022-01-02 22:14:19 +0100

    refactor

--- dice.js
+++ dice.js
@@ -12,6 +12,13 @@ let clicks = 0;
 let randomNumber = 0;
 
 
+function actions(rn,cl,obj){
+	imageDiv.innerHTML = `<img src="./svg/dice${rn}.svg" alt="dice${rn}" width="100">`;
+	counterDiv.innerHTML = `<p>This was the ${cl}. roll.</p>`;
+	list.append(obj);
+
+}
+
 function restart() {
 	clicks = 0;
 	imageDiv.innerHTML = "";
@@ -35,38 +42,8 @@ function roll() {
 		restartButton.style.visibility = "visible"
 	}
 
-	if (randomNumber === 1) {
-		imageDiv.innerHTML = `<img src="./svg/dice1.svg" alt="dice1" width="100">`;
-		counterDiv.innerHTML = `<p>This was the ${clicks}. roll.</p>`;
-		list.append(el);
 
-	}
-	else if (randomNumber === 2) {
-		imageDiv.innerHTML = `<img src="./svg/dice2.svg" alt="dice2" width="100">`;
-		counterDiv.innerHTML = `<p>This was the ${clicks}. roll.</p>`;
-		list.append(el);
-	}
-	else if (randomNumber === 3) {
-		imageDiv.innerHTML = `<img src="./svg/dice3.svg" alt="dice3" width="100">`;
-		counterDiv.innerHTML = `<p>This was the ${clicks}. roll.</p>`;
-		list.appendChild(el);
-	}
-	else if (randomNumber === 4) {
-		imageDiv.innerHTML = `<img src="./svg/dice4.svg" alt="dice4" width="100">`;
-		counterDiv.innerHTML = `<p>This was the ${clicks}. roll.</p>`;
-		list.appendChild(el);
-	}
-	else if (randomNumber === 5) {
-		imageDiv.innerHTML = `<img src="./svg/dice5.svg" alt="dice5" width="100">`;
-		counterDiv.innerHTML = `<p>This was the ${clicks}. roll.</p>`;
-		list.appendChild(el);
-	}
-	else if (randomNumber === 6) {
-		imageDiv.innerHTML = `<img src="./svg/dice6.svg" alt="dice6" width="100">`;
-		counterDiv.innerHTML = `<p>This was the ${clicks}. roll.</p>`;
-		list.appendChild(el);
-	}
-
+	actions(randomNumber, clicks, el);
 
 }
 
